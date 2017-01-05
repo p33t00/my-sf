d791f095c9be615e3d1a35415255e8d353081344
Peet <p33tsjash41k@hotmail.com>
2017-01-05 16:04:40 +0300

    Wasted Time. Backup Before gettin rid of sonata admin

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p33t
+ * Date: 05/01/17
+ * Time: 00:48
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
